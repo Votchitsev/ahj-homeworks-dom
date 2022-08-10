e85c774e2a69c84bb8474bb414fa7ab45299cad2
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-08-10 11:42:44 +0200

    add loaders

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       {
         test: /\.css$/i,
         use: [
-          MiniCssExtractPlugin.loader,
+          'style-loader',
           'css-loader',
         ],
       },
